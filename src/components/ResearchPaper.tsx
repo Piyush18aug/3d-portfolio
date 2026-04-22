60a0d70d65b3cb2177a0fc1836cf1aa99e68720d
piyush pramod navangul <piyushnavangul12@gmail.com>
2026-04-22 17:27:08 +0530

    Fix research issue

--- src/components/ResearchPaper.tsx
+++ src/components/ResearchPaper.tsx
@@ -6,7 +6,7 @@ const ResearchPaper = () => {
     <div
       className="career-section section-container"
       id="research"
-      style={{ position: "relative", backgroundColor: "#020617", zIndex: 11, paddingBottom: "20px", marginBottom: "0px" }}
+      style={{ position: "relative", backgroundColor: "#020617", zIndex: 11, paddingBottom: "150px", marginBottom: "0px" }}
     >
       <div className="career-container">
         <h2>
